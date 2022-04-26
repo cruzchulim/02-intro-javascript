d2431c137dd25a3330c03ccec44a2bf6918ce65c
Cruz Chulim <chulimcruz@gmail.com>
2022-04-26 13:59:33 -0500

    final de las bases de javascript

---
+++ src/Bases/12-ternario.js
@@ -0,0 +1,13 @@
+// operador condicional ternario
+const activo = true;
+// const mensaje  = (activo) ? 'activo':'inactivo';
+const mensaje  = (activo) && 'Activo';
+// let mensaje  = '';
+
+// if(activo){
+//     mensaje = 'activo';
+// }else{
+//     mensaje = 'inactivo';
+// }
+
+console.log(mensaje)
